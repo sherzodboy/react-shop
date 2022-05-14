6bf70edc291e7f5d173dc28e4be783807116dd7e
sherzod02 <kholmamatovsh09>
2022-05-14 17:47:07 +0500

    add reducer

--- src/context.js
+++ src/context.js
@@ -1,10 +1,37 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import { reducer } from "./reducer";
 
 export const ShopContext = createContext();
 
+const initialState = {
+  goods: [],
+  loading: true,
+  order: [],
+  isBasketShow: false,
+};
+
 export const ContextProvider = ({ children }) => {
-  const value = {
-    example: "Hello From Context",
+  const [value, dispatch] = useReducer(reducer, initialState);
+
+  value.addToBasket = (item) => {
+    dispatch({ type: "ADD_TO_BASKET", payload: item });
   };
+
+  value.incrementQuantity = (itemID) => {
+    dispatch({ type: "INCREMENT_QUANTITY", payload: { id: itemID } });
+  };
+
+  value.decrementQuantity = (itemID) => {
+    dispatch({ type: "DECREMENT_QUANTITY", payload: { id: itemID } });
+  };
+
+  value.handleBasketShow = () => {
+    dispatch({ type: "TOGGLE_BASKET" });
+  };
+
+  value.removeFromBasket = (itemID) => {
+    dispatch({ type: "REMOVE_FROM_BASKET", payload: { id: itemID } });
+  };
+
   return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
 };
